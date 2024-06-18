fb858d7c3da4f9f012a0e8a3e0be068afb376038
Keanu Carnevale <keanucarnevale@gmail.com>
2024-06-18 14:18:26 +1200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: "Keanu's portfolio",
+  description: 'an intro to next.js by making a portfolio',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
